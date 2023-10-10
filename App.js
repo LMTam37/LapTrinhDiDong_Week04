beaece875cb049134cacdbd1f4fb5afb642f4d7a
MinhTam <lait2553@gmail.com>
2023-10-10 07:00:53 +0700

    feat: Add ScreenTiki_Ok component

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import Screen2_a from './screens/Screen2_a'
 import Screen2_b from './screens/Screen2_b'
 import Screen2_c from './screens/Screen2_c';
+import ScreenTiki_Ok from './screens/ScreenTiki_Ok';
 
 export default function App() {
   return (
     // <Screen2_a></Screen2_a>
     // <Screen2_b></Screen2_b>
-    <Screen2_c></Screen2_c>
+    // <Screen2_c></Screen2_c>
+    <ScreenTiki_Ok></ScreenTiki_Ok>
   );
 }
